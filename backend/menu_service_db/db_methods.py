7d5b3e229a1b079f7124cd5d577aa737b419bdf2
Sergey Gavrielov <sergey.gav82@gmail.com>
2025-12-03 11:59:22 +0200

    commited get_dish_ingredients

--- backend/menu_service_db/db_methods.py
+++ backend/menu_service_db/db_methods.py
@@ -1,5 +1,7 @@
 import sqlite3
 import os
+from http.client import responses
+# from tkinter.tix import Select
 
 DATABASE_PATH = "menu_service.db"
 
@@ -46,8 +48,34 @@ def delete_dish(dish_id):
     pass
 
 
-def get_dish_ingredients(dish_id):
-    pass
+
+
+
+def get_dish_ingredients(dish_id:int):
+    query = f"""SELECT DishIngredients.Dish_ID, Ingredients.Ingredient_ID, Ingredients.Ingredient_Name, DishIngredients.Ingredient_Quantity, Ingredients.Ingredient_Unit
+FROM DishIngredients
+JOIN Ingredients
+ON DishIngredients.Ingredient_ID = Ingredients.Ingredient_ID
+WHERE DishIngredients.Dish_ID = {dish_id}"""
+
+    conn = sqlite3.connect(DATABASE_PATH)
+    cursor = conn.cursor()
+    response = cursor.execute(query)
+    fetched_res = response.fetchall()
+
+
+    for row in fetched_res:
+        dish_ingredients = []
+        dish_ingredients.append({
+            "Dish_ID": row[0],
+            "Ingredient_ID": row[1],
+            "Ingredient_Name": row[2],
+            "Ingredient_Quantity": row[3],
+            "Ingredient_Unit": row[4]
+        })
+        print(dish_ingredients)
+    return dish_ingredients
+
 
 
 def get_dish_dietary_attributes(dish_id):
@@ -88,3 +116,6 @@ if __name__ == '__main__':
     conn = sqlite3.connect(DATABASE_PATH)
     cursor = conn.cursor()
     conn.close()
+
+
+get_dish_ingredients(3)
